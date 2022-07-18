5bdfb665baa979ff1436ff593773ef17606b6743
jimboweb <jimboweb@gmail.com>
2022-07-18 14:49:23 -0700

    set body and nav dark mode color

--- script.js
+++ script.js
@@ -1,7 +1,17 @@
 let setDark = function(){
+    let body = document.querySelector("body");
+    body.style.backgroundColor="black";
     let main = document.querySelector("main");
     main.style.backgroundColor = "black";
     main.style.color = "white";
+    main.style.borderColor="white";
+    let nav = document.querySelector("nav");
+    nav.style.backgroundColor = "blue";
+    let navLinks = document.querySelectorAll("nav>div>a");
+    navLinks.forEach(function(lnk){
+        lnk.classList.remove("menuLink");
+        lnk.classList.add("menuLinkDark");
+    });
 }
 
 let dmButton = document.querySelector("button#darkMode");
